29cc9d745c1fefb7589664e92957b3f5ba21b971
Alexey Guriev <guriev06@gmail.com>
2022-08-13 23:11:36 +0300

    Postgres & stuff

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_08_13_153704) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
