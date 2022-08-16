f81cdff3a96c817b99da77650aff003043da823a
Alexey Guriev <guriev06@gmail.com>
2022-08-17 01:28:11 +0300

    Validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:real_name, :nickname, :email)
-    @user = User.create(user_params)
+    user_params = params.require(:user).permit(:real_name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    redirect_to root_path, notice: 'Регистрация успешно завершена!'
+    if @user.save
+      redirect_to root_path, notice: 'Регистрация успешно завершена!'
+    else
+      flash.now[:alert] = 'Неверно заполнены поля формы'
+      render :new
+    end
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: /[\w\d]+@[a-z]+\.[a-z]+/ }
+  validates :nickname, presence: true, uniqueness: true, format: { with: /[\w\d]+/ }
 end
