e8719b738d8e6503105532927a0de7b8288c60fa
Alexey Guriev <guriev06@gmail.com>
2022-08-18 00:52:42 +0300

    Post-registration auto-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: 'Регистрация успешно завершена!'
     else
       flash.now[:alert] = 'Неверно заполнены поля формы'
